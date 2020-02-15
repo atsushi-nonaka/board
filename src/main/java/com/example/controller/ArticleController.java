5858f271da73709cc68fa0d9190f7656679abc6d
atsushi-nonaka <atsushi.nonaka@rakus-partners.co.jp>
2020-02-15 16:56:04 +0900

    DBに登録されている記事を全件表示させるプログラムを実装しました

---
+++ src/main/java/com/example/controller/ArticleController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Article;
+import com.example.repository.ArticleRepository;
+
+/**
+ * 記事情報を操作する.
+ * 
+ * @author nonaa
+ *
+ */
+@Controller
+@RequestMapping("")
+public class ArticleController {
+	
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	@RequestMapping("")
+	public String index(Model model) {
+		List<Article> articleList = articleRepository.findAll();
+		model.addAttribute("articleList", articleList);
+		return "board/board";
+	}
+}
---
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -0,0 +1,47 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Article;
+
+/**
+ * 記事のデータベースを操作する.
+ * 
+ * @author nonaa
+ *
+ */
+@Repository
+public class ArticleRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	/** テーブル名 */
+	private final static String TABLE_NAME = "articles";
+	
+	/** 記事情報をセットするローマッパー */
+	private final static RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) ->{
+		Article article = new Article();
+		article.setId(rs.getInt("id"));
+		article.setName(rs.getString("name"));
+		article.setContent(rs.getString("content"));
+		return article;
+	};
+	
+	/**
+	 * 記事の全件検索を行う.
+	 * 
+	 * @return 記事リスト
+	 */
+	public List<Article> findAll(){
+		String sql = "SELECT id, name, content FROM " + TABLE_NAME + " ORDER BY id DESC";
+		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
+		return articleList;
+	}
+	
+}
